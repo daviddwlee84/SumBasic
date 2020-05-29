82bdde2cb5d3b6910b97280569779aa4724d07f3
David Lee <daviddwlee84@gmail.com>
2020-05-30 03:38:50 +0800

    Add BLEU and fix matching bug

--- our_sumbasic.py
+++ our_sumbasic.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from parse_data import DATA_DIR, OUTPUT_DIR, DATAPATH
 from typing import List, Tuple, Dict
 from ast import literal_eval as make_tuple
+import nltk
 
 method = sys.argv[1]
 
@@ -29,7 +30,28 @@ DATA_TO_TEST = {
     'Motivation': 'motivation/test.txt.oracle'
 }
 
+TEST_PAPER = {key: value.replace('oracle', 'src') for key, value in DATA_TO_TEST.items()}
+PAPER_REF = {key: value.replace('oracle', 'ref') for key, value in DATA_TO_TEST.items()}
 
+def get_id_match() -> Dict[str, List[int]]:
+    """ some topic might not exist in the entire test set,
+    as we only store the sample which has at least one gold,
+    some sample might be skipped. so we have to match them """
+
+    with open(os.path.join(DATAPATH, PAPER_REF['Overall']), 'r') as stream:
+        test_ref = stream.readlines()
+    ref_to_id = {ref.strip(): index for index, ref in enumerate(test_ref)}
+
+    topic_id_to_id = {}
+
+    for topic, ref_file in PAPER_REF.items():
+        with open(os.path.join(DATAPATH, ref_file), 'r') as stream:
+            topic_ref = stream.readlines()
+        
+        topic_id_to_id[topic] = [ref_to_id[ref.strip()] for ref in topic_ref]
+    
+    return topic_id_to_id
+    
 def clean_sentence(tokens):
     # print('Cleaning sentences...')
     tokens = [t.lower() for t in tokens]
@@ -210,8 +232,52 @@ def calculate_performance(predicts: List[Tuple[int]], golds: List[Tuple[int]]):
         'f1': f1
     }
 
+def compute_bleu_raw(gold_sents: List[Tuple[str]], predict_sents: List[Tuple[str]], gold: List[Tuple[int]], predict: List[Tuple[int]]):
+    """ input raw sentences (precision)
+    https://www.nltk.org/api/nltk.translate.html
+    https://www.nltk.org/_modules/nltk/translate/bleu_score.html
+    https://www.nltk.org/_modules/nltk/align/bleu_score.html
+    https://stackoverflow.com/questions/32395880/calculate-bleu-score-in-python/39062009
+    """
+    # for avg_sent_bleu
+    avg_sent_bleu = 0
+    chencherry = nltk.translate.bleu_score.SmoothingFunction()
 
-def eval_write_output(summaries: List[List[str]], sent_ids: List[List[int]], scores: List[List[int]], sent_amount: List[int]):
+    # for corpus_bleu
+    all_references = []
+    all_hypothesis = []
+
+    for gs, ps, gid, pid in zip(gold_sents, predict_sents, gold, predict):
+
+        gs = [sent for sent, _ in sorted(zip(gs, gid), key=lambda pair: pair[1])]
+        ps = [sent for sent, _ in sorted(zip(ps, pid), key=lambda pair: pair[1])]
+
+        # TODO: .split() can be replace with any other tokenizer
+        references = [sent.split() for sent in gs]  # TODO: whether concat sentences
+        hypothesis = [word for sent in ps for word in sent.split()]
+
+        # for avg_sent_bleu
+        try:
+            avg_sent_bleu += nltk.translate.bleu_score.sentence_bleu(references, hypothesis,
+                                                            smoothing_function=chencherry.method1)
+        except:
+            import ipdb; ipdb.set_trace()
+        
+        # for corpus_bleu
+        all_references.append(references)
+        all_hypothesis.append(hypothesis)
+
+    # corpus_bleu() is different from averaging sentence_bleu() for hypotheses
+    # bleu = nltk.translate.bleu_score.corpus_bleu(all_references, all_hypothesis,
+    #                                              smoothing_function=chencherry.method1)
+    corpus_bleu = nltk.translate.bleu_score.corpus_bleu(all_references, all_hypothesis)
+    avg_sent_bleu /= len(gold_sents)
+
+    return {'corpus_bleu': corpus_bleu, 'avg_sent_bleu': avg_sent_bleu}
+
+
+
+def eval_write_output(summaries: List[List[str]], sent_ids: List[List[int]], topic_id_to_id: Dict[str, List[int]], scores: List[List[int]], sent_amount: List[int]):
     """ evaluate and write sentence prediction and score """
     print('Writting output...')
     if os.path.exists(OUTPUT_DIR):
@@ -248,9 +314,22 @@ def eval_write_output(summaries: List[List[str]], sent_ids: List[List[int]], sco
         with open(os.path.join(DATAPATH, test_file), 'r') as stream:
             raw_labels = stream.readlines()
         labels = [make_tuple(raw_label) for raw_label in raw_labels]
-        performance = calculate_performance(sent_ids, labels)
-        print(topic, performance)
-        performance_fp.write(f'{topic} {performance}\n')
+
+        pred_ids = [sent_ids[topic_id_to_id[topic][i]] for i in range(len(raw_labels))]
+        performance = calculate_performance(pred_ids, labels)
+
+        with open(os.path.join(DATAPATH, TEST_PAPER[topic]), 'r') as stream:
+            raw_papers = stream.readlines()
+        papers = [paper.strip().split('##SENT##') for paper in raw_papers]
+
+        gold_sents = [[papers[i][index] for index in gold_label] for i, gold_label in enumerate(labels)]
+        # TODO: somehow the prediction index exceed range
+        predict_sents = [[papers[i][index] for index in predict_label if index < len(papers[i])] for i, predict_label in enumerate(pred_ids)]
+
+        bleu_performance = compute_bleu_raw(gold_sents, predict_sents, labels, sent_ids)
+
+        print(topic, performance, bleu_performance)
+        performance_fp.write(f'{topic} {performance} {bleu_performance}\n')
 
     total_sents = sum(sent_amount)
     print(f'Total sentences: {total_sents}')
@@ -280,9 +359,10 @@ def main():
         summaries.append(summary)
         sent_amount.append(num_sentences)
 
+    topic_id_to_id = get_id_match()
     sent_ids = summaries_to_sent_ids(papers, summaries)
 
-    eval_write_output(summaries, sent_ids, scores, sent_amount)
+    eval_write_output(summaries, sent_ids, topic_id_to_id, scores, sent_amount)
 
 
 if __name__ == '__main__':
